f4dfc2a4c79f9f330325bb2b0be3dfec23fdebce
Ramy13015 <abes.ramy@proton.me>
2025-11-01 04:22:52 +0100

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,9 +1,11 @@
 # Fichier: app/config.py
-
 import os
+# Il n'y a pas besoin d'importer conf ou load_dotenv,
+# car Render gère les variables d'environnement.
 
-# Récupère les variables d'environnement de Render
-TELEGRAM_BOT_TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-TELEGRAM_CHAT_ID = os.environ.get("TELEGRAM_CHAT_ID")
+# Utilisation de os.environ.get() pour récupérer les variables
+TELEGRAM_BOT_TOKEN = os.environ.get("TEL_BOT_TOKEN") 
+TELEGRAM_CHAT_ID = os.environ.get("TEL_CHAT_ID")
+# Note : Les noms des variables doivent correspondre exactement à ceux que vous avez sur Render (TEL_BOT_TOKEN, TEL_CHAT_ID).
 
-# Ligne FAST = uvicorn.run(...) DÉFINITIVEMENT SUPPRIMÉE
+# Suppression de toutes les autres lignes (conf.options, FAST=uvicorn.run, etc.)
